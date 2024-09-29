57ed9d6aa9ccd660955bc238a38428e11ccdf4f7
Navesh Jaiswal <119146351+Navesh-J@users.noreply.github.com>
2024-09-29 10:31:53 +0530

    Time: 2 ms (93.01%), Space: 41.9 MB (67.31%) - LeetHub

---
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxNumberOfBalloons(String s) {
+        String target="balloon";
+        int countS[]=new int[26];
+        int countT[]=new int[26];
+        for(char ch:s.toCharArray()){
+            int index=ch-'a';
+            countS[index]++;
+        }
+        for(char ch:target.toCharArray()){
+            int index=ch-'a';
+            countT[index]++;
+        }
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<26;i++){
+            if(countT[i]>0){
+                int poss=(int)(countS[i]/countT[i]);
+                min=Math.min(min,poss);
+            }
+        }
+        return min;
+    }
+}
