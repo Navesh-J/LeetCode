b17cb0c3d0db191168bcf13555657b10df6fbcc5
Navesh Jaiswal <119146351+Navesh-J@users.noreply.github.com>
2024-10-10 19:26:00 +0530

    Time: 1 ms (89.92%), Space: 40.5 MB (60.22%) - LeetHub

---
+++ 2650-split-with-minimum-sum/2650-split-with-minimum-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int splitNum(int num) {
+        char[] digits = Integer.toString(num).toCharArray();
+        Arrays.sort(digits);
+        int num1 = 0, num2 = 0;
+        for (int i = 0; i < digits.length; i++) {
+            if (i % 2 == 0) {
+                num1 = num1 * 10 + (digits[i] - '0');
+            } else {
+                num2 = num2 * 10 + (digits[i] - '0');
+            }
+        }
+        return num1 + num2;
+    }
+}
