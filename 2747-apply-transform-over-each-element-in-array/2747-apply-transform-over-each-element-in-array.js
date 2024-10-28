39508fedec6d360d5a6fd5c845d6f20917b3b38f
Navesh Jaiswal <119146351+Navesh-J@users.noreply.github.com>
2024-10-28 18:11:52 +0530

    Time: 50 ms (67.02%), Space: 49.1 MB (14.07%) - LeetHub

---
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+        let a=[];
+        for(let i=0;i<arr.length;i++)
+        a[i]=fn(arr[i],i)
+        return a;
+};
