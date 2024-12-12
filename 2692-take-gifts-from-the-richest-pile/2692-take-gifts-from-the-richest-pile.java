72765a925dab8c6c2536c696133f2728d76525da
Navesh Jaiswal <119146351+Navesh-J@users.noreply.github.com>
2024-12-12 16:13:31 +0530

    Time: 6 ms (65.07%), Space: 42.1 MB (92.06%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.java
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.java
@@ -1,17 +1,24 @@
 class Solution {
     public long pickGifts(int[] gifts, int k) {
         long res=0;
-        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)-> (b-a));
-        for(int gift:gifts){
-            pq.offer(gift);
-        }
         while(k-->0){
-            int num=(int)Math.floor(Math.sqrt(pq.poll()));
-            pq.offer(num);
+            int max=max(gifts);
+            int num=gifts[max];
+            num=(int)Math.floor(Math.sqrt(num));
+            gifts[max]=num;
         }
-        while(!pq.isEmpty()){
-            res+=pq.poll();
+        for(int gift:gifts){
+            res+=gift;
         }
         return res;
+    }
+    private int max(int[] gifts){
+        int max=0;
+        for(int i=0;i<gifts.length;i++){
+            if(gifts[i]>gifts[max]){
+                max=i;
+            }
+        }
+        return max;
     }
 }
