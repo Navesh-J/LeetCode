d7daa1f7cd0949f50d032c537e9814ebf7f66a80
Navesh Jaiswal <119146351+Navesh-J@users.noreply.github.com>
2024-12-27 14:25:08 +0530

    Time: 4 ms (89.46%), Space: 53.7 MB (24.59%) - LeetHub

---
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxScoreSightseeingPair(int[] values) {
+        int max=0;
+        int start=values[0];
+        for(int j=1;j<values.length;j++){
+            max=Math.max(max,start+values[j]-j);
+            start=Math.max(start,values[j]+j);
+        }
+        return max;
+    }
+}
