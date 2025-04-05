61544573a3e1a942524cc58b76587cef3373d562
Navesh Jaiswal <119146351+Navesh-J@users.noreply.github.com>
2025-04-05 12:57:24 +0530

    Time: 42 ms (26.79%), Space: 55.8 MB (69.08%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,30 +1,35 @@
 class Solution {
-    public int[] sortArray(int[] arr) {
-        int n=arr.length;
-        for(int i=n/2-1;i>=0;i--){
-            heapify(arr, n, i);
-        }
+    public int[] sortArray(int[] nums) {
+        buildHeap(nums);
+        int n=nums.length;
         for(int i=n-1;i>=0;i--){
-            int temp=arr[0];
-            arr[0]=arr[i];
-            arr[i]=temp;
-            heapify(arr, i,0);
+            int temp=nums[0];
+            nums[0]=nums[i];
+            nums[i]=temp;
+            maxHeap(nums,i,0);
         }
-        return arr;
+        return nums;
     }
-    void heapify(int arr[],int n,int i){
+    private void maxHeap(int arr[],int n,int i){
         int largest=i;
-        int l=2*i+2;
-        int r=2*i+1;
-        if(l<n && arr[l]>arr[largest])
-        largest=l;
-        if(r<n && arr[r]>arr[largest])
-        largest=r;
-        if (largest!=i) {
+        int left=2*i+1, right=2*i+2;
+        if(left<n && arr[left]>arr[largest])
+            largest=left;
+        if(right<n && arr[right]>arr[largest])
+            largest=right;
+        if(largest != i){
             int temp=arr[i];
             arr[i]=arr[largest];
             arr[largest]=temp;
-            heapify(arr, n, largest);
+            maxHeap(arr,n,largest);
         }
     }
+
+    private void buildHeap(int arr[]){
+        int n=arr.length;
+        for(int i=(n-2)/2;i>=0;i--){
+            maxHeap(arr,n,i);
+        }
+    }
+
 }
