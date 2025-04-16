bab9ad0c0fa2b6d35a19cb124e9a33384f77df84
Navesh Jaiswal <119146351+Navesh-J@users.noreply.github.com>
2025-04-16 19:04:16 +0530

    Time: 0 ms (100%), Space: 41.6 MB (26.1%) - LeetHub

--- 2412-minimum-amount-of-time-to-fill-cups/2412-minimum-amount-of-time-to-fill-cups.java
+++ 2412-minimum-amount-of-time-to-fill-cups/2412-minimum-amount-of-time-to-fill-cups.java
@@ -1,26 +1,7 @@
 class Solution {
-    public int fillCups(int[] arr) {
-        int res=0;
-        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
-        for(int num:arr){
-            if(num!=0)
-                pq.offer(num);
-        }
-        while(!pq.isEmpty()){
-            while(pq.contains(0))
-                pq.remove(0);
-            if(pq.isEmpty()) break;
-            if(pq.size() > 1){
-                res++;
-                int a = pq.poll();
-                int b = pq.poll();
-                pq.offer(a-1);
-                pq.offer(b-1);
-            }else if(pq.size() == 1){
-                res++;
-                pq.offer(pq.poll()-1);
-            }
-        }
-        return res;
+    public int fillCups(int[] amount) {
+        int max = Math.max(amount[0], Math.max(amount[1], amount[2]));
+        int sum = amount[0] + amount[1] + amount[2];
+        return Math.max(max, (sum + 1) / 2);
     }
 }
