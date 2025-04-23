82f2b32807f14fbb7c74a5c1fa48bcbdb68eb0ab
Navesh Jaiswal <119146351+Navesh-J@users.noreply.github.com>
2025-04-23 15:37:07 +0530

    Time: 5 ms (73.12%), Space: 40.1 MB (98.21%) - LeetHub

--- 1500-count-largest-group/1500-count-largest-group.java
+++ 1500-count-largest-group/1500-count-largest-group.java
@@ -1,15 +1,15 @@
 import java.util.HashMap;
 class Solution {
     public int countLargestGroup(int n) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int arr[]=new int[40];
         int max = 0;
         for (int i = 1; i <= n; i++) {
             int sum = sod(i);
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
-            max = Math.max(max, map.get(sum));
+            arr[sum]++;
+            max = Math.max(max, arr[sum]);
         } 
         int res = 0;
-        for (int val : map.values()) {
+        for (int val : arr) {
             if (val == max) res++;
         }  
         return res;
