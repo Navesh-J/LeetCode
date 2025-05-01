fd313ea20ed21b52cd0a94c5436c4ff9e3543477
Navesh Jaiswal <119146351+Navesh-J@users.noreply.github.com>
2025-05-01 23:16:50 +0530

    Time: 1 ms (100%), Space: 42.6 MB (94.5%) - LeetHub

--- 3684-substring-matching-pattern/3684-substring-matching-pattern.java
+++ 3684-substring-matching-pattern/3684-substring-matching-pattern.java
@@ -1,14 +1,9 @@
 class Solution {
     public boolean hasMatch(String s, String p) {
-        String[] parts = p.split("\\*", -1);
-        String p1 = parts[0], p2 = parts[1];
-        int i;
-        int i1=-1,i2=-1;
-        if(s.contains(p1))
-            i1 = s.indexOf(p1);
-        if(s.contains(p2))
-            i2 = s.lastIndexOf(p2);
-        if((i1+p1.length())<=i2 && i1!=-1) return true;
-        return false;
+      int i =p.indexOf("*");
+      int firstPat=s.indexOf(p.substring(0,i));
+      int secondPat = s.indexOf(p.substring(i + 1), firstPat + i); 
+      if(firstPat !=-1 && secondPat!=-1 ){return true;}
+      return false;
     }
 }
