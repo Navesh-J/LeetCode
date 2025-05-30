8243de173935c7cca1e3e59171371931d594b585
Navesh Jaiswal <119146351+Navesh-J@users.noreply.github.com>
2025-05-30 21:52:09 +0530

    Time: 1 ms (99.57%), Space: 42.4 MB (30.24%) - LeetHub

--- 1720-crawler-log-folder/1720-crawler-log-folder.java
+++ 1720-crawler-log-folder/1720-crawler-log-folder.java
@@ -1,18 +1,15 @@
 class Solution {
     public int minOperations(String[] logs) {
-        int res=0;
-        Stack<String> st = new Stack<>();
+        int count=0;
         for(String log:logs){
-            if(log.equals("./")) continue;
-            if(st.isEmpty() && log.equals("../")) continue;
             if(log.equals("../")){
-                st.pop();
-                res--;
-            }else{
-                st.push(log);
-                res++;
+                if (count>0){
+                    count--;
+                }
+            }else if(!log.equals("./")){
+                count++;
             }
         }
-        return res;
+        return count;
     }
 }
