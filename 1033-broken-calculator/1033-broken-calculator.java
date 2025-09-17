7584660458630c8947118cd867587ed5ccb63e4b
Navesh Jaiswal <119146351+Navesh-J@users.noreply.github.com>
2025-09-17 16:49:19 +0530

    Time: 0 ms (100%), Space: 40.2 MB (95.38%) - LeetHub

---
+++ 1033-broken-calculator/1033-broken-calculator.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int brokenCalc(int startValue, int target) {
+        int res=0;
+        while(target > startValue){
+            if(target%2 == 0){
+                target/=2;
+                res++;
+            }else{
+                res++;
+                target+=1;
+            }
+        }
+        return res+(startValue-target);
+    }
+}
