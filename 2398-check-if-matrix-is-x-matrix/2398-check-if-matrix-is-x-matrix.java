0824efbf9b9a39b5089061e8746fc021ea2c892b
Navesh Jaiswal <119146351+Navesh-J@users.noreply.github.com>
2026-09-03 00:31:53 +0530

    Time: 3 ms (24.21%), Space: 47.5 MB (15.49%) - LeetHub

---
+++ 2398-check-if-matrix-is-x-matrix/2398-check-if-matrix-is-x-matrix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkXMatrix(int[][] grid) {
+        int n = grid.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                boolean isDiagonal = (i == j) || (i + j == n - 1);
+
+                if (isDiagonal && grid[i][j] == 0) {
+                    return false;
+                }
+
+                if (!isDiagonal && grid[i][j] != 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
